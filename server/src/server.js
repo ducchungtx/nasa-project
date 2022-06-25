6ca45cf93303c2635cef58a39aba7056c93b9266
Chung Nguyen <zrmedia9@gmail.com>
2022-06-25 20:54:50 +0700

    Load init data and script to load one time BE, FE

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,18 @@
 // Config for express server nodejs
 const http = require('http');
-const PORT = process.env.PORT || 8000;
+
+const { loadPlanetsData } = require('./models/planets.model');
 
 const app = require('./app');
 
+const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+  await loadPlanetsData();
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+startServer();
