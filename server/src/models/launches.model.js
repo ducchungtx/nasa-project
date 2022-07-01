5b7d7a35c8897d51983d9bc45b287c095b3a5ffa
Chung Nguyen <zrmedia9@gmail.com>
2022-07-01 21:18:19 +0700

    Done nasa project with static file db

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,35 +1,51 @@
 const launches = new Map();
 
-let lastestFlightNumber = 100;
+let latestFlightNumber = 100;
 
 const launch = {
   flightNumber: 100,
   mission: 'Kepler Exploration X',
   rocket: 'Explorer IS1',
   launchDate: new Date('December 27, 2030'),
-  destination: 'Kepler-442 b',
-  customers: ['NASA', 'SpaceX', 'Google'],
+  target: 'Kepler-442 b',
+  customer: ['ZTM', 'NASA'],
   upcoming: true,
   success: true,
 };
 
 launches.set(launch.flightNumber, launch);
 
+function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
+}
+
 function getAllLaunches() {
   return Array.from(launches.values());
 }
 
 function addNewLaunch(launch) {
-  lastestFlightNumber++;
-  launches.set(lastestFlightNumber, Object.assign(launch, {
-    success: true,
-    upcoming: true,
-    flightNumber: lastestFlightNumber,
-    customers: ["NASA", "ZTM"],
-  }));
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ['Zero to Mastery', 'NASA'],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
 }
 
 module.exports = {
+  existsLaunchWithId,
   getAllLaunches,
-  addNewLaunch
-}
+  addNewLaunch,
+  abortLaunchById,
+};
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,5 @@
-const { getAllLaunches, addNewLaunch } = require('../../models/launches.model');
+const { getAllLaunches, addNewLaunch, existsLaunchWithId,
+  abortLaunchById, } = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
   // * Map convert to json "launches"
@@ -8,15 +9,15 @@ function httpGetAllLaunches(req, res) {
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
 
-  if(!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
+  if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target) {
     return res.status(400).json({
       error: 'Bad Request',
-      message: 'Mission, rocket, launchDate, destination are required'
+      message: 'Mission, rocket, launchDate, target are required'
     });
   }
 
   launch.launchDate = new Date(launch.launchDate);
-  if(isNaN(launch.launchDate.getTime())) {
+  if (isNaN(launch.launchDate.getTime())) {
     return res.status(400).json({
       error: 'Bad Request',
       message: 'Invalid date'
@@ -26,7 +27,22 @@ function httpAddNewLaunch(req, res) {
   return res.status(201).json(launch);
 }
 
+function httpAbortLaunch(req, res) {
+  const launchId = Number(req.params.id);
+
+  if (!existsLaunchWithId(launchId)) {
+    return res.status(404).json({
+      error: 'Launch not found',
+    });
+  }
+
+  const aborted = abortLaunchById(launchId);
+  return res.status(200).json(aborted);
+}
+
+
 module.exports = {
   httpGetAllLaunches,
-  httpAddNewLaunch
+  httpAddNewLaunch,
+  httpAbortLaunch
 }
