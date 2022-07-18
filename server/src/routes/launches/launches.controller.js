04d1ade8c642aec8aaef81e516e9e1792820e7ba
Chung Nguyen <zrmedia9@gmail.com>
2022-07-18 10:42:04 +0700

    Done to query in launches, remove unused code

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,8 +1,13 @@
 const { getAllLaunches, scheduleNewLaunch, existsLaunchWithId, abortLaunchById } = require('../../models/launches.model');
 
+const {
+  getPagination,
+} = require('../../services/query');
+
 async function httpGetAllLaunches(req, res) {
-  console.log('req.query', req.query);
-  return res.status(200).json(await getAllLaunches());
+  const { skip, limit } = getPagination(req.query);
+  const launches = await getAllLaunches(skip, limit);
+  return res.status(200).json(launches);
 }
 
 async function httpAddNewLaunch(req, res) {
@@ -38,7 +43,7 @@ async function httpAbortLaunch(req, res) {
   }
 
   const aborted = abortLaunchById(launchId);
-  if(!aborted) {
+  if (!aborted) {
     return res.status(400).json({
       error: 'Bad Request',
       message: 'Launch not aborted'
